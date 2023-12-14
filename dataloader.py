ba6d8010d3ebf7722b23037f76030f9ac806c1a9
Zahid Tushar <ztushar1@ada.cm.cluster>
2023-12-14 15:58:23 -0500

    update

--- dataloader.py
+++ dataloader.py
@@ -1,6 +1,7 @@
 from torchvision import transforms
 import matplotlib.pyplot as plt
-
+from torch.utils.data import Dataset, random_split, DataLoader
+import torch
 # image transform
 '''
 resize, horizontal flip, 
@@ -34,10 +35,17 @@ if __name__=="__main__":
         images = [preprocess(image.convert("RGB")) for image in examples["image"]]
         return {"images": images}
 
-    fig, axs = plt.subplots(1, 4, figsize=(16, 4))
-    for i, image in enumerate(dataset[:4]["image"]):
-        axs[i].imshow(image)
-        axs[i].set_axis_off()
-    # fig.show()
-    plt.savefig("demo.png")
-    plt.close()
+    # fig, axs = plt.subplots(1, 4, figsize=(16, 4))
+    # for i, image in enumerate(dataset[:4]["image"]):
+    #     axs[i].imshow(image)
+    #     axs[i].set_axis_off()
+    # # fig.show()
+    # plt.savefig("demo3.png")
+    # plt.close()
+    dataset.set_transform(transform)
+    loader = DataLoader(dataset, batch_size=2,shuffle=False)
+    for step, batch in enumerate(loader):
+        clean_images = batch["images"]
+        print(clean_images.shape)
+        print(torch.min(clean_images),torch.max(clean_images))
+        break
